e65f35537b6d5c20ccf6d9f1e2492e499785a473
Jesús Felipe <jesus_system_07@hotmail.com>
2014-03-14 20:04:11 -0500

    Google Maps - Node.

---
+++ index.js
@@ -0,0 +1,10 @@
+/*Node.js es un sistema de eventos basado en javascript, el cual se basa en dependencias 
+* en la carpeta node_modules del proyecto se almacena todas las dependencias descargadas
+*/
+
+/*Requiero la dependencia llamada express
+* Express: Es un Framework para programar en node.js, te hace la vida mas facil.
+*/
+var express = require("express");
+/*Creando un server https*/
+var app = express("http");
